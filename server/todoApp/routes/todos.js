38730aac51fff76850aa1b68c3760e0abb619d1e
Thomas Babu <tommy.developer@gmail.com>
2015-02-02 00:47:06 +0800

    Added all other CURD endpoints

--- server/todoApp/routes/todos.js
+++ server/todoApp/routes/todos.js
@@ -12,4 +12,40 @@ router.get('/', function(req, res, next) {
   });	  
 });
 
+/* POST todo */
+router.post('/', function(req, res, next) {
+	Todo.create(req.body, function(err, post) {
+		if(err) return next(err);
+
+		res.json(post);
+	});
+});
+
+/* GET /todos/:id */
+router.get('/:id', function(req, res, next){
+	Todo.findById(req.params.id, function(err,post) {
+		if(err) return next(err);
+
+		res.json(post);
+	});
+});
+
+/* PUT /todos/:id */
+router.put('/:id', function(req, res, next){
+	Todo.findByIdAndUpdate(req.params.id, req.body, function(err, post) {
+		if(err) return next(err);
+
+		res.json(post);
+	});
+});
+
+/* DELETE /todos/:id */
+router.delete('/:id', function(req, res, next) {
+	Todo.findByIdAndRemove(req.params.id, req.body, function(err, post) {
+		if(err)	return next(err);
+		res.json(post);
+	});
+});
+
+
 module.exports = router;
