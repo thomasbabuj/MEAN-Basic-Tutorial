32845a43015867d426cabf24c0266693dc355314
Thomas Babu <tommy.developer@gmail.com>
2015-02-02 00:24:36 +0800

    Including the todos module

--- server/todoApp/app.js
+++ server/todoApp/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var todos = require('./routes/todos');
 
 var app = express();
 
